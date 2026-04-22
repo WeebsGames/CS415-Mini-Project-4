26e246d51dae99ab9fbc98a51bb8191e803ef8ab
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-04-22 14:05:25 -0500

    started work on P2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 img = np.ones((500, 500, 3), dtype=np.uint8) * 255
@@ -33,4 +34,45 @@ cv2.imwrite('output.png', img)
 
 cv2.imshow('Transformed Circles', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# img = cv2.imread("lena.png")
+# rows, cols, ch, = img.shape
+
+# translation = np.array([[1,0],
+#                        [0,1],
+#                        [100,200]])
+
+# translated = cv2.warpAffine(img, translation, (cols, rows))
+img = cv2.imread('lena.png')
+rows, cols, ch = img.shape
+
+pts1 = np.float32([[50, 50],
+                   [200, 50], 
+                   [50, 200]])
+
+pts2 = np.float32([[10, 100],
+                   [200, 50], 
+                   [100, 250]])
+
+M = cv2.getAffineTransform(pts1, pts2)
+dst = cv2.warpAffine(img, M, (cols, rows))
+
+plt.subplot(121)
+plt.imshow(img)
+plt.title('Input')
+
+plt.subplot(122)
+plt.imshow(dst)
+plt.title('Output')
+
+plt.show()
+
+# Displaying the image
+while(1):
+    
+    cv2.imshow('image', img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+        
 cv2.destroyAllWindows()
